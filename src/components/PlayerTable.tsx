ffaef73aaaf9ef10526d005a6c4012870aee280c
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-05-28 21:41:34 +0300

    Added Alert for each id

--- src/components/PlayerTable.tsx
+++ src/components/PlayerTable.tsx
@@ -5,28 +5,25 @@ type IProps = {
 };
 
 export const PlayerTable = (props: IProps) => {
+
+
   return (
     <table className="table-auto">
       <thead>
         <tr>
           <th className="text-right px-5">Id</th>
           <th className="text-right px-5">Name</th>
-          <th className="text-right px-5">Number</th>
           <th className="text-right px-5">Team</th>
-          <th className="text-right px-5">Age</th>
-          <th className="text-right px-5">Position</th>
         </tr>
       </thead>
 
       <tbody>
         {props.players.map((player, index) => (
-          <tr key={index}>
+          <tr key={index} className="hover:bg-gray-300 cursor-pointer" onClick={() => 
+          { alert(`Click' ${player.id}`) }}>
             <td className="text-right px-5">{player.id}</td>
             <td className="text-right px-5">{player.name}</td>
-            <td className="text-right px-5">{player.number}</td>
             <td className="text-right px-5">{player.team}</td>
-            <td className="text-right px-5">{player.age}</td>
-            <td className="text-right px-5">{player.position}</td>
           </tr>
         ))}
       </tbody>
